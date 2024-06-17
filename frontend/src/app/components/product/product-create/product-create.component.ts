bad83bf72b073247135e9da729fcdc62641c59a4
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-06-17 18:13:57 -0300

    feat(frontend): criando o botão de adicionar produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.scss'
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
