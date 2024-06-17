2c04b1e58ef50a6c5d32d94f9cc2f0a394d83460
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-06-17 12:36:29 -0300

    feat(frontend): criando o component product-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from './views/home/home.component'
 import { MatCardModule } from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { RedDirective } from './directives/red.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
