815fb847f627bce0771b9c46eecbff00b463df2b
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-07-04 18:09:25 -0300

    feat(frontend): adicionando coluna de ações: details, edit, delete

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -11,7 +11,7 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit{
 
   products: Product[] = []; // Inicializar como array vazio
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
   constructor(private productService: ProductService) { }
 
