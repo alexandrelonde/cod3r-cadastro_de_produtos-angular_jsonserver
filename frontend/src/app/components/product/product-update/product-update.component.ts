dd9931cadd999ee2bb66c0d4d781d7f6de27aca2
Alexandre Londe <alexandrelonde.x@gmail.com>
2024-07-08 15:37:19 -0300

    feat(frontend): tela e lógica de update

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
   styleUrl: './product-update.component.scss'
 })
-export class ProductUpdateComponent {
+export class ProductUpdateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id) {
+      this.productService.readById(id).subscribe(product => {
+        this.product = product;
+      });
+    } else {
+      console.error('ID is null');
+    }
+  }
+
+  /*
+    No método acima, veio um erro mostrando que id poderia ser nulo.
+    Eu teria a alternativa ou de afirmar que id não seria nulo por
+    meio de "!" ou tratar o erro.
+    De fato o id não virá nulo por meio da consulta, mas para ficar
+    uma solução mais perspicaz preferi trar o erro com "if".
+  */
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+
+  }
+
+  cancel():void {
+    this.router.navigate(['/products']);
+  }
 
 }
